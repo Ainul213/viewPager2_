f495d5d13524a6575981643ae4e1d14ea4a49f6f
Ainul213 <ainulkuno@gmail.com>
2024-12-05 15:39:52 +0700

    Tugas recyclerviw

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
+
+fun s() = "com.google.android.material:material:1.8.0"
--- app/src/main/java/com/example/viewpager2/FriendsFragment.kt
+++ app/src/main/java/com/example/viewpager2/FriendsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,9 @@ class FriendsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var friendAdapter: MyFriendAdapter
+    private val friend = mutableListOf<MyFriend>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +35,26 @@ class FriendsFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false)
+        val view = inflater.inflate(R.layout.fragment_friends, container, false)
+        recyclerView = view.findViewById(R.id.recyclerview)
+        setupRecyclerView()
+        return view
+    }
+    private fun setupRecyclerView(){
+        friend.addAll(
+            listOf(
+                MyFriend(name = "ai", phone = "08423123346", email = "ai@gamil.com"),
+                MyFriend(name = "do", phone = "088753457834", email = "do@gamil.com"),
+                MyFriend(name = "leh", phone = "087545735344", email = "leh@gamil.com")
+            )
+        )
+        friendAdapter = MyFriendAdapter(friend)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = friendAdapter
     }
 
     companion object {
